e208bc58c4b361b32e2621746cdbad0c4f3c81cd
Sutton <jackevansutton@gmail.com>
2025-09-26 11:08:47 -0400

    Added Running, But Ran Into Errors

--- Prototype2/Assets/Scripts/PlayerController.cs
+++ Prototype2/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float horizontalInput;
+    public float speed = 10.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis(“Horizontal”);
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
