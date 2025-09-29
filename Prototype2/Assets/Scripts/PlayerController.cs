6206f1efb6d485724d0ce581921061420a1512d5
Sutton <jackevansutton@gmail.com>
2025-09-29 10:55:20 -0400

    Made Player Stay Inside Boundaries

--- Prototype2/Assets/Scripts/PlayerController.cs
+++ Prototype2/Assets/Scripts/PlayerController.cs
@@ -8,12 +8,16 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x < -10)
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
