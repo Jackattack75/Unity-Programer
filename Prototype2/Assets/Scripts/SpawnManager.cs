b50f574b43032dde4fca9308e014684619809eda
Sutton <jackevansutton@gmail.com>
2025-10-01 11:24:17 -0400

    Randomize the spawn location

--- Prototype2/Assets/Scripts/SpawnManager.cs
+++ Prototype2/Assets/Scripts/SpawnManager.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,8 +15,10 @@ public class SpawnManager : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.S)) {
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),
+            0, spawnPosZ);
             int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20),
+            Instantiate(animalPrefabs[animalIndex], spawnPos ,
             animalPrefabs[animalIndex].transform.rotation);
         }
     }
