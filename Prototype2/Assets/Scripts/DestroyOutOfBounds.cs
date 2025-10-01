0ce05c106aee43f4fa5e7da0ed78b048943ee0c1
Sutton <jackevansutton@gmail.com>
2025-10-01 10:56:11 -0400

    Fixed Steak Issue When Leaving Map

--- Prototype2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Prototype2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
+    private float lowerBound = -10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,6 +15,8 @@ public class DestroyOutOfBounds : MonoBehaviour
     {
         if (transform.position.z > topBound)
         {
+            Destroy(gameObject);
+        }   else if (transform.position.z < lowerBound) {
             Destroy(gameObject);
         }
     }
