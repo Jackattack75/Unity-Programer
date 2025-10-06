894d3f7e003dc3582bf667f853f140ad8c6fcc5e
Sutton <jackevansutton@gmail.com>
2025-10-06 11:00:45 -0400

    Added "Game Over" Message

--- Prototype2/Assets/Scripts/DetectCollisions.cs
+++ Prototype2/Assets/Scripts/DetectCollisions.cs
@@ -10,19 +10,12 @@ public class DetectCollisions : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    { }
 
-    }
-
-
-
-    void OnTriggerEnter(Collider other)
-    {
+    void OnTriggerEnter(Collider other) {
         Destroy(gameObject);
         Destroy(other.gameObject);
 
     }
-
-
 
 }
