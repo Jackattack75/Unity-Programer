bfc9e4c1d79aba6a4560c7cc197f9cf58cdd19c4
Sutton <jackevansutton@gmail.com>
2025-10-21 10:46:08 -0400

    3.2.6 - Stop obstacle spawning on gameOver - Fixed issue from previous commit

--- Prototype3/Prototype3/Assets/Scripts/MoveLeft.cs
+++ Prototype3/Prototype3/Assets/Scripts/MoveLeft.cs
@@ -8,14 +8,13 @@ public class MoveLeft : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playerControllerScript =
-        GameObject.Find("Player").GetComponent<PlayerController>();
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (playerControllerScript.gameOver == false)
+        if (playerControllerScript.gameOver == false) 
         {
             transform.Translate(Vector3.left * Time.deltaTime * speed);
         }
