8a4fcf3d78330b407ed9d7698916e329b7a89fb1
Sutton <jackevansutton@gmail.com>
2025-10-21 11:14:32 -0400

    3.3.2 - Make the player start off at a run

--- Prototype3/Prototype3/Assets/Scripts/MoveLeft.cs
+++ Prototype3/Prototype3/Assets/Scripts/MoveLeft.cs
@@ -5,7 +5,7 @@ public class MoveLeft : MonoBehaviour
     private float speed = 30;
     private PlayerController playerControllerScript;
     private float leftBound = -15;
-    
+    // added animation
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
