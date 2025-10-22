4448c9d44ea73ba61f5bd1ff2ff0c5a834e01a63
Sutton <jackevansutton@gmail.com>
2025-10-22 11:04:31 -0400

    3.3.4 - Adjust the jump animation

--- Prototype3/Prototype3/Assets/Scripts/PlayerController.cs
+++ Prototype3/Prototype3/Assets/Scripts/PlayerController.cs
@@ -17,7 +17,7 @@ public class PlayerController : MonoBehaviour
         Physics.gravity = new Vector3(0, -9.81f * gravityModifier, 0);
     }
 
-    // Update is called once per frame
+    // Update is called once per frame.
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
