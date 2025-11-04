eb0d939a5aa059bfdb81221563a775f5de573eed
Sutton <jackevansutton@gmail.com>
2025-11-04 10:47:53 -0500

    4.3.3 - Test for enemy and powerup

--- Prototype4/Assets/Scripts/PlayerController.cs
+++ Prototype4/Assets/Scripts/PlayerController.cs
@@ -34,7 +34,13 @@ public class PlayerController : MonoBehaviour
 
     }
 
-
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Debug.Log("Collided with " +  collision.gameObject.name + " with powerup set to " + hasPowerup);
+        }
+    }
 
 
 
