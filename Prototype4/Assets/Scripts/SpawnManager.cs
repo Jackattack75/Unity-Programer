9dcd78534f94685217208bef4a090b2888d44c48
Sutton <jackevansutton@gmail.com>
2025-11-05 11:10:58 -0500

    4.4.2 - Give the for-loop a parameter

--- Prototype4/Assets/Scripts/SpawnManager.cs
+++ Prototype4/Assets/Scripts/SpawnManager.cs
@@ -8,7 +8,7 @@ public class SpawnManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        SpawnEnemyWave();
+        SpawnEnemyWave(3);
     }
 
     // Update is called once per frame
@@ -19,9 +19,9 @@ public class SpawnManager : MonoBehaviour
 
     }
 
-    void SpawnEnemyWave()
+    void SpawnEnemyWave(int enemiesToSpawn)
     {
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < enemiesToSpawn; i++)
         {
             Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
 
