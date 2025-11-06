ece6e27941072a5bdaf8fe8dd4e540a0ffe3581d
Sutton <jackevansutton@gmail.com>
2025-11-06 10:45:14 -0500

    Finalized Game - Finished Unit-4

--- Prototype4/Assets/Scripts/SpawnManager.cs
+++ Prototype4/Assets/Scripts/SpawnManager.cs
@@ -2,12 +2,23 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public static SpawnManager Instance;
     public GameObject enemyPrefab;
     private float spawnRange = 9;
     public int enemyCount;
     public int waveNumber = 1;
     public GameObject powerupPrefab;
 
+    void Awake()
+    {
+        // Ensure only one SpawnManager exists
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -31,7 +42,7 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnEnemyWave(int enemiesToSpawn)
     {
-        for (int i = 0; i < enemiesToSpawn; i++)
+        for (int i = 1; i < enemiesToSpawn; i++)
         {
             Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
 
@@ -42,8 +53,8 @@ public class SpawnManager : MonoBehaviour
 
     private Vector3 GenerateSpawnPosition ()
     {
-        float spawnPosX = Random.Range(spawnRange, spawnRange);
-        float spawnPosZ = Random.Range(spawnRange, spawnRange);
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
         return randomPos;
 
